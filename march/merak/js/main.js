1a194a1d44edf08b488c8757f7d7e79f911032e8
Oleh-99 <cyaputa99@gmail.com>
2021-04-16 17:09:59 +0300

    add edit

--- march/merak/js/main.js
+++ march/merak/js/main.js
@@ -41,22 +41,38 @@
 	}
 
 	function slider() {
-		$('.swiper-container').each(function(e) {
-			var $thisData = $(this).data('option');
-			new Swiper(this, renderSlider($thisData));
+		var $swipers = $('.swiper-container');
+		var $galleryThumbs;
+
+		if ($swipers.length === 0) {
+			return;
+		};
+
+		if ('undefined' === typeof Swiper) {
+			return;
+		};
+
+		$swipers.each(function(e) {
+			var $this = $(this);
+			var $thisData = $this .data('option');
+			new Swiper(this, renderSlider($thisData, $this));
+			console.log(renderSlider($thisData, $this));
+			if($swipers.hasClass('gallery-thumbs')) {
+				$galleryThumbs = new Swiper(this, renderSlider($thisData, $this));
+			}
 		})
 
-		function renderSlider(data) {
+		function renderSlider(data, thisSlider) {
 			var config = {
 				loop : data.loop,
 				slidesPerView : data.slidesPerView,
 			};
 	
-			if(data.spaceBetween) {
+			if (data.spaceBetween) {
 				config.spaceBetween = data.spaceBetween;
 			};
 
-			if(data.pagination) {
+			if (data.pagination) {
 				config.pagination = {
 					el: '.slider-numbers .swiper-pagination',
 					clickable: true,
@@ -66,14 +82,14 @@
 				};
 			};
 
-			if(data.navigation) {
+			if (data.navigation) {
 				config.navigation = {
 					nextEl: '.slider-right',
 					prevEl: '.slider-left',
 				  };
 			};
 
-			if(config.slidesPerView > 1) {
+			if (config.slidesPerView > 1) {
 				config.breakpoints = {
 					993: {
 						slidesPerView: config.slidesPerView,
@@ -83,13 +99,38 @@
 					},
 				};
 			};
+
+			if (data.loopedSlides) {
+				config.loopedSlides = data.loopedSlides;
+			}
+
+			if (thisSlider.hasClass('gallery-thumbs')) {
+				config.watchSlidesVisibility = true,
+				config.watchSlidesProgress = true
+			}
+
+			if (thisSlider.hasClass('gallery-top')) {
+				config.thumbs = {
+					swiper: $galleryThumbs
+				};
+			}
 	
 			return config;
 		};
 	}
 
 	function quickView() {
-		$('.button-search').magnificPopup({
+		var $btnSearch = $('.button-search');
+
+		if ('undefined' === typeof $btnSearch.magnificPopup) {
+			return;
+		};
+
+		if ($btnSearch.length === 0) {
+			return;
+		};
+		
+		$btnSearch.magnificPopup({
 			type: 'inline',
 			removalDelay: 500,
 			callbacks: {
@@ -102,11 +143,11 @@
 	}
 
 	function filterProduct() {
-		if($('.shop-product-grid').length === 0) {
+		if ($('.shop-product-grid').length === 0) {
 			return;
 		};
 
-		if('undefined' === typeof Isotope) {
+		if ('undefined' === typeof Isotope) {
 			return;
 		};
 
@@ -131,15 +172,22 @@
 	}
 
 	function waypointer() {
-		if('undefined' === typeof Waypoint) {
+		var $animated = $('.with-animated');
+
+		if ('undefined' === typeof Waypoint) {
 			return;
 		};
 
-		$('.with-animated').each(function() {
+		if ($animated.length === 0) {
+			return;
+		};
+
+		$animated.each(function() {
 			var $this = $(this);
+
 			new Waypoint({
-				element: (this),
-				handler: function(direction) {		
+				element: this,
+				handler: function() {		
 					$this.addClass('animate');
 				},
 				offset: '100%',
@@ -148,17 +196,59 @@
 	}
 
 	function tooltip() {
-		$('[data-toggle="tooltip"]').tooltip()
+		var $btnTooltip = $('[data-toggle="tooltip"]');
+
+		if ('undefined' === typeof tooltip) {
+			return;
+		};
+
+		if ($btnTooltip.length === 0) {
+			return;
+		};
+
+		$btnTooltip.tooltip();
 	}
 
-$(document).ready(function() {
-	productTabs();
-	toTop();
-	slider();
-	quickView();
-	filterProduct();
-	waypointer();	
-	tooltip();
-});
+	function maps() {
+		if ('undefined' === typeof Maplace) {
+			return;
+		};
+
+		if ($('#gmap').length === 0) {
+			return;
+		};
+
+		var data = [{
+			lat: 55.679224,
+			lon: 12.583471,
+			title: 'København',
+			html: '<h3>København</h3>',
+			zoom: 17,
+		},{
+			lat: 56.155002,
+			lon: 10.185312,
+			title: 'Aarhus',
+			html: '<h3>Aarhus</h3>',
+			zoom: 17,
+		}];
+
+		new Maplace({
+			locations: data,
+			controls_type: 'list',
+			controls_on_map: true,
+			start: 1,
+		}).Load();
+	}
+
+	$(document).ready(function() {
+		productTabs();
+		toTop();
+		slider();
+		quickView();
+		filterProduct();
+		waypointer();	
+		tooltip();
+		maps();
+	});
 
 })(jQuery);
