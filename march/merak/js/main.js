c5cc2721bdfb57bbb5e7fe56de95f749284c4835
Oleh-99 <cyaputa99@gmail.com>
2021-04-16 12:30:44 +0300

    add edit

--- march/merak/js/main.js
+++ march/merak/js/main.js
@@ -6,6 +6,7 @@
 
 			$tabsButton.on('click', function (e) {
 				e.preventDefault();
+
 				var $this = $(this);
 				var $buttonData = $this.data('value');
 				var $tabContent = $productWrapper.find(`.product-main-wrapper[data-value='${$buttonData}']`);
@@ -20,18 +21,21 @@
 	}
 
 	function toTop() {
-		var toTop = $('.to-top')
-		$(window).scroll(function () {
+		var toTop = $('.to-top');
+
+		$(document).scroll(function () {
 			if ($(this).scrollTop() > 300) {
 				toTop.fadeIn();
 			} else {
 				toTop.fadeOut();
 			}
 		});
+
 		toTop.click(function () {
 			$('body,html').animate({
 				scrollTop: 0
 			}, 400);
+
 			return false;
 		});
 	}
@@ -41,43 +45,47 @@
 			var $thisData = $(this).data('option');
 			new Swiper(this, renderSlider($thisData));
 		})
-	}
 
-	function renderSlider(data) {
-		var config = {};
-		config.loop = data.loop;
-		config.slidesPerView = data.slidesPerView;
+		function renderSlider(data) {
+			var config = {
+				loop : data.loop,
+				slidesPerView : data.slidesPerView,
+			};
+	
+			if(data.spaceBetween) {
+				config.spaceBetween = data.spaceBetween;
+			};
 
-		if(data.spaceBetween) {
-			config.spaceBetween = data.spaceBetween;
-		}		
-		if(data.pagination) {
-			config.pagination={
-				el: '.slider-numbers .swiper-pagination',
-				clickable: true,
-				renderBullet: function (index, className) {
-					return '<span class="' + className + '">' + (index + 1) + '</span>';
-				},
-			}
+			if(data.pagination) {
+				config.pagination = {
+					el: '.slider-numbers .swiper-pagination',
+					clickable: true,
+					renderBullet: function (index, className) {
+						return '<span class="' + className + '">' + (index + 1) + '</span>';
+					},
+				};
+			};
+
+			if(data.navigation) {
+				config.navigation = {
+					nextEl: '.slider-right',
+					prevEl: '.slider-left',
+				  };
+			};
+
+			if(config.slidesPerView > 1) {
+				config.breakpoints = {
+					993: {
+						slidesPerView: config.slidesPerView,
+					},
+					150: {
+						slidesPerView: 1,
+					},
+				};
+			};
+	
+			return config;
 		};
-		if(data.navigation) {
-			config.navigation= {
-				nextEl: '.slider-right',
-				prevEl: '.slider-left',
-			  }
-		};
-		if(config.slidesPerView > 1) {
-			config.breakpoints={
-				993: {
-					slidesPerView: config.slidesPerView,
-				},
-				150: {
-					slidesPerView: 1,
-				},
-			}
-		}
-
-		return config;
 	}
 
 	function quickView() {
@@ -94,29 +102,53 @@
 	}
 
 	function filterProduct() {
+		if($('.shop-product-grid').length === 0) {
+			return;
+		};
+
+		if('undefined' === typeof Isotope) {
+			return;
+		};
+
 		let grid = new Isotope('.shop-product-grid', {
 			itemSelector: '.filter-grid',
 			layoutMode: 'fitRows'
 		});
+
 		$('.product-menu').on('click', 'li', function(e) {
 			e.preventDefault();
+
 			let $this = $(this);
+			let filterData = $this.data('filter');
+
 			$this.siblings().removeClass('active');
 			$this.addClass('active');
-			let filterData = $this.data('filter');
+			
 			grid.arrange({
 				filter: filterData
 			});
 		});
 	}
 
-	function waypont() {
-		var waypoint = new Waypoint({
-			element: document.getElementById('basic-waypoint'),
-			handler: function() {
-			  notify('Basic waypoint triggered')
-			}
-		  })
+	function waypointer() {
+		if('undefined' === typeof Waypoint) {
+			return;
+		};
+
+		$('.with-animated').each(function() {
+			var $this = $(this);
+			new Waypoint({
+				element: (this),
+				handler: function(direction) {		
+					$this.addClass('animate');
+				},
+				offset: '100%',
+			  });
+		});
+	}
+
+	function tooltip() {
+		$('[data-toggle="tooltip"]').tooltip()
 	}
 
 $(document).ready(function() {
@@ -124,7 +156,9 @@ $(document).ready(function() {
 	toTop();
 	slider();
 	quickView();
-	filterProduct();	
+	filterProduct();
+	waypointer();	
+	tooltip();
 });
 
 })(jQuery);
